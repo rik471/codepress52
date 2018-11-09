5f67e8308f496a2546a0bd5c41ced001e77f4111
Rick.py <riksongleison471@gmail.com>
2018-11-09 18:45:09 -0200

    pacote codepress integrado com codedatabase

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -7,6 +7,6 @@ Route::group(['middleware' => ['web']], function () {
 
 Route::group(['middleware' => 'web'], function (){
     Route::auth();
-    Route::get('/home', 'HomeController@index');
+    Route::get('/', 'HomeController@index');
 });
 
